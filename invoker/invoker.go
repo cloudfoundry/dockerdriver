94a1eaf355cc6ccd5deec975f954c9c56c37d84b
Luke Woydziak <luke.woydziak@emc.com>
2016-11-30 11:38:21 -0800

    use invoker in nfsdriver to standardize CLI access [#135247503](https://www.pivotaltracker.com/story/show/135247503)

---
+++ invoker/invoker.go
@@ -0,0 +1,52 @@
+package invoker
+
+import (
+	"code.cloudfoundry.org/voldriver"
+	"code.cloudfoundry.org/goshims/execshim"
+	"code.cloudfoundry.org/lager"
+)
+
+//go:generate counterfeiter -o ./cephfakes/fake_invoker.go . Invoker
+
+type Invoker interface {
+	Invoke(env voldriver.Env, executable string, args []string) error
+}
+
+type realInvoker struct {
+	useExec execshim.Exec
+}
+
+func NewRealInvoker() Invoker {
+	return NewRealInvokerWithExec(&execshim.ExecShim{})
+}
+
+func NewRealInvokerWithExec(useExec execshim.Exec) Invoker {
+	return &realInvoker{useExec}
+}
+
+func (r *realInvoker) Invoke(env voldriver.Env, executable string, cmdArgs []string) error {
+	logger := env.Logger().Session("invoking-command", lager.Data{"executable": executable, "args": cmdArgs})
+	logger.Info("start")
+	defer logger.Info("end")
+
+	cmdHandle := r.useExec.CommandContext(env.Context(), executable, cmdArgs...)
+
+	_, err := cmdHandle.StdoutPipe()
+	if err != nil {
+		logger.Error("unable to get stdout", err)
+		return err
+	}
+
+	if err = cmdHandle.Start(); err != nil {
+		logger.Error("starting command", err)
+		return err
+	}
+
+	if err = cmdHandle.Wait(); err != nil {
+		logger.Error("command-exited", err)
+		return err
+	}
+
+	// could validate stdout, but defer until actually need it
+	return nil
+}
---
+++ invoker/invoker_test.go
@@ -0,0 +1,67 @@
+package invoker_test
+
+import (
+	"bytes"
+	"context"
+	"fmt"
+
+	"code.cloudfoundry.org/goshims/execshim/exec_fake"
+	"code.cloudfoundry.org/lager"
+	"code.cloudfoundry.org/lager/lagertest"
+	"code.cloudfoundry.org/voldriver"
+	"code.cloudfoundry.org/voldriver/driverhttp"
+
+	"code.cloudfoundry.org/voldriver/invoker"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("RealInvoker", func() {
+	var (
+		subject    invoker.Invoker
+		fakeCmd    *exec_fake.FakeCmd
+		fakeExec   *exec_fake.FakeExec
+		testLogger lager.Logger
+		testCtx    context.Context
+		testEnv    voldriver.Env
+		cmd        = "some-fake-command"
+		args       = []string{"fake-args-1"}
+	)
+	Context("when invoking an executable", func() {
+		BeforeEach(func() {
+			testLogger = lagertest.NewTestLogger("InvokerTest")
+			testCtx = context.TODO()
+			testEnv = driverhttp.NewHttpDriverEnv(testLogger, testCtx)
+			fakeExec = new(exec_fake.FakeExec)
+			fakeCmd = new(exec_fake.FakeCmd)
+			fakeExec.CommandContextReturns(fakeCmd)
+			subject = invoker.NewRealInvokerWithExec(fakeExec)
+		})
+
+		It("should report an error when unable to attach to stdout", func() {
+			fakeCmd.StdoutPipeReturns(errCloser{bytes.NewBufferString("")}, fmt.Errorf("unable to attach to stdout"))
+			err := subject.Invoke(testEnv, cmd, args)
+			Expect(err).To(HaveOccurred())
+			Expect(err.Error()).To(Equal("unable to attach to stdout"))
+		})
+
+		It("should report an error when unable to start binary", func() {
+			fakeCmd.StdoutPipeReturns(errCloser{bytes.NewBufferString("cmdfails")}, nil)
+			fakeCmd.StartReturns(fmt.Errorf("unable to start binary"))
+			err := subject.Invoke(testEnv, cmd, args)
+			Expect(err).To(HaveOccurred())
+			Expect(err.Error()).To(Equal("unable to start binary"))
+		})
+		It("should report an error when executing the driver binary fails", func() {
+			fakeCmd.WaitReturns(fmt.Errorf("executing driver binary fails"))
+
+			err := subject.Invoke(testEnv, cmd, args)
+			Expect(err).To(HaveOccurred())
+			Expect(err.Error()).To(Equal("executing driver binary fails"))
+		})
+		It("should successfully invoke cli", func() {
+			err := subject.Invoke(testEnv, cmd, args)
+			Expect(err).ToNot(HaveOccurred())
+		})
+	})
+})
