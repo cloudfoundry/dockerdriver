9216dda918a24bcdb6c4dc3093e441a53e48d4b9
Paul Warren <paul.warren@emc.com>
2017-08-04 10:42:23 -0700

    Add a helper function ReadDriverSpec
    [#149934726](https://www.pivotaltracker.com/story/show/149934726)
    
    Signed-off-by: Kai Xiang <kxiang@pivotal.io>

--- driver_spec.go
+++ driver_spec.go
@@ -1,7 +1,12 @@
 package voldriver
 
 import (
+	"bufio"
+	"encoding/json"
+	"fmt"
 	"os"
+	"path"
+	"strings"
 
 	"code.cloudfoundry.org/lager"
 )
@@ -26,4 +31,63 @@ func WriteDriverSpec(logger lager.Logger, pluginsDirectory string, driver string
 	}
 	f.Sync()
 	return nil
+}
+
+func ReadDriverSpec(logger lager.Logger, specName string, driverPath string, specFile string) (*DriverSpec, error) {
+	logger = logger.Session("read-driver-spec", lager.Data{"spec-name": specName, "spec-file": specFile})
+	logger.Info("start")
+	defer logger.Info("end")
+
+	var driverSpec DriverSpec
+
+	if strings.Contains(specFile, ".") {
+		extension := strings.Split(specFile, ".")[1]
+		switch extension {
+		case "sock":
+			driverSpec = DriverSpec{
+				Name:    specName,
+				Address: path.Join(driverPath, specFile),
+			}
+		case "spec":
+			configFile, err := os.Open(path.Join(driverPath, specFile))
+			if err != nil {
+				logger.Error("error-opening-config", err, lager.Data{"DriverFileName": specFile})
+				return nil, err
+			}
+			reader := bufio.NewReader(configFile)
+			addressBytes, _, err := reader.ReadLine()
+			if err != nil { // no real value in faking this as bigger problems exist when this fails
+				logger.Error("error-reading-driver-file", err, lager.Data{"DriverFileName": specFile})
+				return nil, err
+			}
+			driverSpec = DriverSpec{
+				Name:    specName,
+				Address: string(addressBytes),
+			}
+		case "json":
+			// extract url from json file
+			var driverJsonSpec DriverSpec
+			configFile, err := os.Open(path.Join(driverPath, specFile))
+			if err != nil {
+				logger.Error("error-opening-config", err, lager.Data{"DriverFileName": specFile})
+				return nil, err
+			}
+			jsonParser := json.NewDecoder(configFile)
+			if err = jsonParser.Decode(&driverJsonSpec); err != nil {
+				logger.Error("parsing-config-file-error", err)
+				return nil, err
+			}
+			driverSpec = DriverSpec{
+				Name:      specName,
+				Address:   driverJsonSpec.Address,
+				TLSConfig: driverJsonSpec.TLSConfig,
+			}
+		default:
+			err := fmt.Errorf("unknown-driver-extension: %s", extension)
+			logger.Error("driver", err)
+			return nil, err
+		}
+	}
+
+	return &driverSpec, nil
 }
