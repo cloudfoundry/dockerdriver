dc3d1851f471e0dc0bd4cfdffcabe60a446dd72c
Paul Warren <paul.warren@dell.com>
2018-09-17 11:18:27 -0700

    Add utils for working with unique volume IDs
    [#160356605](https://www.pivotaltracker.com/story/show/160356605)
    
    Signed-off-by: Dave Walter <dwalter@pivotal.io>

---
+++ utils/volume_id_test.go
@@ -0,0 +1,29 @@
+package utils_test
+
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+
+	"code.cloudfoundry.org/voldriver/utils"
+)
+
+var _ = Describe("Utils/VolumeId", func() {
+	var (
+		volumeId utils.VolumeId
+	)
+
+	BeforeEach(func() {
+		volumeId = utils.NewVolumeId("some_prefix", "some-suffix")
+	})
+
+	It("should return an appropriately formatted volumeid string", func() {
+		Expect(volumeId.GetUniqueId()).To(Equal("some=prefix_some-suffix"))
+	})
+
+	It("should deserialize appropriately", func() {
+		newVolumeId, err := utils.NewVolumeIdFromEncodedString("other-prefix_other=suffix")
+		Expect(err).NotTo(HaveOccurred())
+
+		Expect(newVolumeId).To(Equal(utils.VolumeId{Prefix: "other-prefix", Suffix: "other_suffix"}))
+	})
+})
