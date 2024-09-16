6183ff8b5f7dde93c764660f3f8e96f4edbe3fb5
Geoff Franks <geoff.franks@broadcom.com>
2024-09-16 18:36:01 +0000

    Catch or explicitly ignore unhandled errors

--- scripts/test-in-docker.bash
+++ scripts/test-in-docker.bash
@@ -3,7 +3,7 @@
 set -eu
 
 THIS_FILE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-CI="${THIS_FILE_DIR}/../../wg-app-platform-runtime-ci"
+CI="${THIS_FILE_DIR}/../../../../../wg-app-platform-runtime-ci"
 . "$CI/shared/helpers/git-helpers.bash"
 REPO_NAME=$(git_get_remote_name)
 
